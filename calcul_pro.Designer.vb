6eae45923fc146cc8c60122b5c06819382f75e00
yassmine-labidi <labidiyassmine793@gmail.com>
2024-11-14 19:05:37 +0100

    Ajoutez des fichiers projet.

---
+++ calcul_pro.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class calcul_pro
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txt = New TextBox()
+        btn_0 = New Button()
+        btn_3 = New Button()
+        btn_9 = New Button()
+        btn_7 = New Button()
+        btn_6 = New Button()
+        btn_8 = New Button()
+        btn_5 = New Button()
+        btn_2 = New Button()
+        btn_4 = New Button()
+        Button9 = New Button()
+        btn_c = New Button()
+        btn_eq = New Button()
+        btn_div = New Button()
+        btn_mult = New Button()
+        btn_min = New Button()
+        btn_plus = New Button()
+        SuspendLayout()
+        ' 
+        ' txt
+        ' 
+        txt.Location = New Point(112, 74)
+        txt.Name = "txt"
+        txt.Size = New Size(263, 27)
+        txt.TabIndex = 0
+        ' 
+        ' btn_0
+        ' 
+        btn_0.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        btn_0.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_0.Location = New Point(71, 130)
+        btn_0.Name = "btn_0"
+        btn_0.Size = New Size(59, 53)
+        btn_0.TabIndex = 1
+        btn_0.Text = "0"
+        btn_0.UseVisualStyleBackColor = False
+        ' 
+        ' btn_3
+        ' 
+        btn_3.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        btn_3.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_3.Location = New Point(372, 130)
+        btn_3.Name = "btn_3"
+        btn_3.Size = New Size(59, 53)
+        btn_3.TabIndex = 2
+        btn_3.Text = "3"
+        btn_3.UseVisualStyleBackColor = False
+        ' 
+        ' btn_9
+        ' 
+        btn_9.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        btn_9.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_9.Location = New Point(172, 275)
+        btn_9.Name = "btn_9"
+        btn_9.Size = New Size(59, 53)
+        btn_9.TabIndex = 3
+        btn_9.Text = "9"
+        btn_9.UseVisualStyleBackColor = False
+        ' 
+        ' btn_7
+        ' 
+        btn_7.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        btn_7.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_7.Location = New Point(372, 199)
+        btn_7.Name = "btn_7"
+        btn_7.Size = New Size(59, 53)
+        btn_7.TabIndex = 4
+        btn_7.Text = "7"
+        btn_7.UseVisualStyleBackColor = False
+        ' 
+        ' btn_6
+        ' 
+        btn_6.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        btn_6.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_6.Location = New Point(279, 199)
+        btn_6.Name = "btn_6"
+        btn_6.Size = New Size(59, 53)
+        btn_6.TabIndex = 5
+        btn_6.Text = "6"
+        btn_6.UseVisualStyleBackColor = False
+        ' 
+        ' btn_8
+        ' 
+        btn_8.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        btn_8.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_8.Location = New Point(71, 275)
+        btn_8.Name = "btn_8"
+        btn_8.Size = New Size(59, 53)
+        btn_8.TabIndex = 6
+        btn_8.Text = "8"
+        btn_8.UseVisualStyleBackColor = False
+        ' 
+        ' btn_5
+        ' 
+        btn_5.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        btn_5.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_5.Location = New Point(172, 199)
+        btn_5.Name = "btn_5"
+        btn_5.Size = New Size(59, 53)
+        btn_5.TabIndex = 7
+        btn_5.Text = "5"
+        btn_5.UseVisualStyleBackColor = False
+        ' 
+        ' btn_2
+        ' 
+        btn_2.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        btn_2.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_2.Location = New Point(279, 130)
+        btn_2.Name = "btn_2"
+        btn_2.Size = New Size(59, 53)
+        btn_2.TabIndex = 8
+        btn_2.Text = "2"
+        btn_2.UseVisualStyleBackColor = False
+        ' 
+        ' btn_4
+        ' 
+        btn_4.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        btn_4.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_4.Location = New Point(71, 199)
+        btn_4.Name = "btn_4"
+        btn_4.Size = New Size(59, 53)
+        btn_4.TabIndex = 9
+        btn_4.Text = "4"
+        btn_4.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Button9.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button9.Location = New Point(172, 130)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(59, 53)
+        Button9.TabIndex = 10
+        Button9.Text = "1"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' btn_c
+        ' 
+        btn_c.BackColor = Color.FromArgb(CByte(255), CByte(224), CByte(192))
+        btn_c.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_c.Location = New Point(267, 275)
+        btn_c.Name = "btn_c"
+        btn_c.Size = New Size(99, 53)
+        btn_c.TabIndex = 11
+        btn_c.Text = "C"
+        btn_c.UseVisualStyleBackColor = False
+        ' 
+        ' btn_eq
+        ' 
+        btn_eq.BackColor = Color.FromArgb(CByte(255), CByte(224), CByte(192))
+        btn_eq.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_eq.Location = New Point(391, 275)
+        btn_eq.Name = "btn_eq"
+        btn_eq.Size = New Size(87, 53)
+        btn_eq.TabIndex = 12
+        btn_eq.Text = "="
+        btn_eq.UseVisualStyleBackColor = False
+        ' 
+        ' btn_div
+        ' 
+        btn_div.BackColor = Color.FromArgb(CByte(255), CByte(224), CByte(192))
+        btn_div.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_div.Location = New Point(391, 358)
+        btn_div.Name = "btn_div"
+        btn_div.Size = New Size(93, 53)
+        btn_div.TabIndex = 13
+        btn_div.Text = "/"
+        btn_div.UseVisualStyleBackColor = False
+        ' 
+        ' btn_mult
+        ' 
+        btn_mult.BackColor = Color.FromArgb(CByte(255), CByte(224), CByte(192))
+        btn_mult.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_mult.Location = New Point(267, 358)
+        btn_mult.Name = "btn_mult"
+        btn_mult.Size = New Size(99, 53)
+        btn_mult.TabIndex = 14
+        btn_mult.Text = "*"
+        btn_mult.UseVisualStyleBackColor = False
+        ' 
+        ' btn_min
+        ' 
+        btn_min.BackColor = Color.FromArgb(CByte(255), CByte(224), CByte(192))
+        btn_min.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_min.Location = New Point(155, 358)
+        btn_min.Name = "btn_min"
+        btn_min.Size = New Size(91, 53)
+        btn_min.TabIndex = 15
+        btn_min.Text = "-"
+        btn_min.UseVisualStyleBackColor = False
+        ' 
+        ' btn_plus
+        ' 
+        btn_plus.BackColor = Color.FromArgb(CByte(255), CByte(224), CByte(192))
+        btn_plus.Font = New Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_plus.Location = New Point(38, 358)
+        btn_plus.Name = "btn_plus"
+        btn_plus.Size = New Size(84, 53)
+        btn_plus.TabIndex = 16
+        btn_plus.Text = "+"
+        btn_plus.UseVisualStyleBackColor = False
+        ' 
+        ' calcul_pro
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(496, 471)
+        Controls.Add(btn_plus)
+        Controls.Add(btn_min)
+        Controls.Add(btn_mult)
+        Controls.Add(btn_div)
+        Controls.Add(btn_eq)
+        Controls.Add(btn_c)
+        Controls.Add(Button9)
+        Controls.Add(btn_4)
+        Controls.Add(btn_2)
+        Controls.Add(btn_5)
+        Controls.Add(btn_8)
+        Controls.Add(btn_6)
+        Controls.Add(btn_7)
+        Controls.Add(btn_9)
+        Controls.Add(btn_3)
+        Controls.Add(btn_0)
+        Controls.Add(txt)
+        Name = "calcul_pro"
+        Text = "Calculatrise"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txt As TextBox
+    Friend WithEvents btn_0 As Button
+    Friend WithEvents btn_3 As Button
+    Friend WithEvents btn_9 As Button
+    Friend WithEvents btn_7 As Button
+    Friend WithEvents btn_6 As Button
+    Friend WithEvents btn_8 As Button
+    Friend WithEvents btn_5 As Button
+    Friend WithEvents btn_2 As Button
+    Friend WithEvents btn_4 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents btn_c As Button
+    Friend WithEvents btn_eq As Button
+    Friend WithEvents btn_div As Button
+    Friend WithEvents btn_mult As Button
+    Friend WithEvents btn_min As Button
+    Friend WithEvents btn_plus As Button
+
+End Class
---
+++ calcul_pro.vb
@@ -0,0 +1,156 @@
+﻿Public Class calcul_pro
+    Dim A As String
+    Dim B As String
+    Dim op = ""
+    Private Sub btn_0_Click(sender As Object, e As EventArgs) Handles btn_0.Click
+        If op = "" Then
+            A = A + "0"
+        Else
+            B = B + "0"
+        End If
+        txt.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If op = "" Then
+            A = A + "1"
+        Else
+            B = B + "1"
+        End If
+        txt.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub btn_2_Click(sender As Object, e As EventArgs) Handles btn_2.Click
+        If op = "" Then
+            A = A + "2"
+        Else
+            B = B + "2"
+        End If
+        txt.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub btn_3_Click(sender As Object, e As EventArgs) Handles btn_3.Click
+        If op = "" Then
+            A = A + "3"
+        Else
+            B = B + "3"
+        End If
+        txt.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub btn_4_Click(sender As Object, e As EventArgs) Handles btn_4.Click
+        If op = "" Then
+            A = A + "4"
+        Else
+            B = B + "4"
+        End If
+        txt.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub btn_5_Click(sender As Object, e As EventArgs) Handles btn_5.Click
+        If op = "" Then
+            A = A + "5"
+        Else
+            B = B + "5"
+        End If
+        txt.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub btn_6_Click(sender As Object, e As EventArgs) Handles btn_6.Click
+        If op = "" Then
+            A = A + "6"
+        Else
+            B = B + "6"
+        End If
+        txt.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub btn_7_Click(sender As Object, e As EventArgs) Handles btn_7.Click
+        If op = "" Then
+            A = A + "7"
+        Else
+            B = B + "7"
+        End If
+        txt.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub btn_8_Click(sender As Object, e As EventArgs) Handles btn_8.Click
+        If op = "" Then
+            A = A + "8"
+        Else
+            B = B + "8"
+        End If
+        txt.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub btn_9_Click(sender As Object, e As EventArgs) Handles btn_9.Click
+        If op = "" Then
+            A = A + "9"
+        Else
+            B = B + "9"
+        End If
+        txt.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub btn_c_Click(sender As Object, e As EventArgs) Handles btn_c.Click
+        txt.Text = ""
+
+    End Sub
+
+    Private Sub btn_eq_Click(sender As Object, e As EventArgs) Handles btn_eq.Click
+        Dim test = True
+        Dim res As Double
+        If IsNumeric(A) And IsNumeric(B) And Not op = "" Then
+            Dim va = Integer.Parse(A)
+            Dim vb = Integer.Parse(B)
+
+
+            If op = "+" Then
+                res = va + vb
+            ElseIf op = "-" Then
+                res = va - vb
+            ElseIf op = "*" Then
+                res = va * vb
+            ElseIf op = "/" And Not op = "" Then
+                res = va / vb
+            Else
+                MessageBox.Show("division par zero")
+                test = False
+            End If
+        Else
+            MessageBox.Show("A et b doit etre entier")
+            test = False
+        End If
+        If test = True Then
+            txt.Text = txt.Text + "=" + res.ToString
+        End If
+    End Sub
+
+    Private Sub btn_plus_Click(sender As Object, e As EventArgs) Handles btn_plus.Click
+        If Not A = "" Then
+            op = "+"
+        End If
+    End Sub
+
+    Private Sub btn_min_Click(sender As Object, e As EventArgs) Handles btn_min.Click
+        If Not A = "" Then
+            op = "-"
+        End If
+    End Sub
+
+    Private Sub btn_mult_Click(sender As Object, e As EventArgs) Handles btn_mult.Click
+        If Not A = "" Then
+            op = "*"
+        End If
+    End Sub
+
+    Private Sub btn_div_Click(sender As Object, e As EventArgs) Handles btn_div.Click
+        If Not A = "" Then
+            op = "/"
+        End If
+    End Sub
+
+    Private Sub txt_TextChanged(sender As Object, e As EventArgs) Handles txt.TextChanged
+
+    End Sub
+End Class
